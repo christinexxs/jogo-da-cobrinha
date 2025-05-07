495db57d4eff0b2b32f12aba37b505a1d33cf3e2
Vick <vitoria.cms.veleiros@gmail.com>
2025-05-07 16:08:31 -0300

    att 07/05/2025

---
+++ script.js
@@ -0,0 +1,51 @@
+/* Play Board é a tela ou tabuleiro */
+/* Conteiner onde a cobra e a comida serão renderizadas */
+const playBoard = document.querySelector(".play-board");
+/* Pontuação atual do jogador */
+const scoreElement = document.querySelector(".score");
+/* Recorde (maiorpontuação) */
+const highScoreElement =  document.querySelector(".high-score");
+/* Controles de movimento */
+/* Seleciona elementos <i> Iconnes Botões para Devices Mobiles */
+const controls = document.querySelectorAll(".controls i");
+
+/* Cadastro de Variaveis */
+
+/* Variaveis Boleana que indica se o jogo terminou */
+let gameOver = false;
+/* variavel para armazenar as coordenadas X e Y DA Comida */
+let foodX, foodY;
+/* Armazena as coordenadas X e Y da cabeça da cobra (posição inicial de 5)*/
+let snakeX = 5, snakeY = 5;
+/*variavel para armazenar a velocidade nas direções X e Y, inicialmente em 0, pq a cobra está parada  */
+let velocityX = 0, velocityY = 0;
+/*uma Array para armazenar as cordenadas de cada segmento do corpo,primeiro elemento é a cabeça */
+let snakeBody = [];
+/*  variavel para armazenar o ID do intervalo que será usado para atualizar para o jogo em um determinado ritmo*/
+let setIntervalId;
+/* uma variavel para manter a pontuação atual do jogador */
+let score = 0;
+
+
+
+/* Obtenha pontuação alta do para armazenamento local */
+/* tenta recuperar o valor associado à  chave "high-score" do arzenamento local do navegador*/
+let highScore = localStorage.getItem("high-score") || 0;
+/* se o localStorage retornar NULL (caso ele não exista), a variavel highscore sera definida como 0 */
+
+
+/* posição aleatoria entre 1 e 30 para a comida  */
+/* gera Coordenadas aleatórias para a nova posição da comida */
+const updateFoodPosition = () => {
+/* +1 : Adicionar 1 para garantir que as coordenadas da comida estejam entre 1 e 30. */
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Mth.floor(Math.random() * 30) + 1;
+}
+
+/* finção para lidar com o Fim do Jogo */
+/* função handleGamaeOver = quando a cobra colide consigo mesma ou com as paredes do tabuleiro */
+
+const handleGamaeOver = () => {
+    clearInterval(setIntervalId);
+    alert("Game Over!!! Aperte Ok para iniciar novamente...")
+    location.reload();}
